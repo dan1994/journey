2daa0993cc77cc1b174ee984c0453aa2a2654a76
Dan Arad <dan1994@gmail.com>
2021-03-20 00:12:45 +0200

    Go to protected mode
    Load GDT, set protected mode, jump to 32 bit entrypoint and set
    segments and stack.
    
    We create a GDT with code and data segments and point segments
    accordingly. We point the ebp and esp to 0x200000.
    
    We also add a make rule for attaching gdb.

--- bootloader/boot.asm
+++ bootloader/boot.asm
@@ -1,4 +1,4 @@
-ORG 0h ; Set offset of addresses to 0 and use segment registers instead
+ORG 7c00h ; Set offset of addresses to where the bootloader is loaded
 BITS 16 ; Work in 16 bit mode
 
 ; BIOS Parameter Block
@@ -13,79 +13,89 @@ bpb:
 times 33 db 0 ; Dummy content for BPB fields
 
 bpb_trampoline:
-    jmp 7c0h:start ; Sets code segment to where the bootloader is loaded
+    jmp 0:start ; Sets code segment to 0
 
-; Main Code
+; Entrypoint
+
+; Calculate GDT segment descriptor offsets (for 32 bit mode)
+CODE_SEGMENT equ gdt_code - gdt_start
+DATA_SEGMENT equ gdt_data - gdt_start
 
 start:
+.setup_segments_and_stack:
     cli ; Clear interrupts
 
-    ; Setup segments
-    mov ax, 7c0h
-    mov ds, ax ; DS = 0x7c00
-    mov es, ax ; ES = 0x7c00
+    ; Setup segments (DS, ES, SS)
+    mov ax, 0
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
 
-    ; Setup stack
-    mov ax, 0h
-    mov ss, ax ; SS = 0x0
+    ; Start stack where we are loaded
     mov sp, 7c00h
 
     sti ; Enable interrupts
 
-    call main
-    jmp $ ; Infinite loop
+.enter_protected_mode:
+    cli
 
-main:
-    ; read_sectors(buffer, offset=2, number=1)
-    mov bx, buffer
-    mov cl, 2h
-    mov al, 1h
-    call read_sectors
+    lgdt[gdt_descriptor] ; Load global descriptor table
 
-    ; if(failed) goto .done
-    cmp ax, 0h
-    jnz .done
+    ; Enable protected mode by setting cr0 lowest bit
+    mov eax, cr0
+    or eax, 1h
+    mov cr0, eax
 
-    ; print(buffer)
-    mov si, buffer
-    call print
-.done:
-    ret
+    jmp CODE_SEGMENT:start32 ; Jump to 32 bit code
 
-; @arg - bx - pointer to buffer
-; @arg - cl - Sector offset
-; @arg - al - Number of sectors
-; @return - ax - 0 iff success
-read_sectors:
-    mov ah, 2h ; Set interrupt to read
-    mov ch, 0h ; Cylinder low eight bits
-    mov dh, 0h ; Head number
-    int 13h
-    jnc .done
-.error:
-    mov si, .error_message
-    call print
-    mov ax, -1
-    ret
-.done:
-    mov ax, 0
-    ret
-.error_message: db 'Failed to read sectors', 0
+; GDT
 
-; @arg - si - point to the string to print
-print:
-    mov bx, 0h ; Set foreground color
-    mov ah, eh ; Set interrupt to print
-.loop:
-    lodsb ; al = si++
-    cmp al, 0h ; Did we reach null?
-    je .done
-    int 10h
-    jmp .loop
-.done:
-    ret
+gdt_start:
+gdt_null:
+    dd 0h
+    dd 0h
+
+; Offset 0x8. Relevant to CS
+gdt_code:
+    dw 0ffffh ; Segment limit 0:15
+    dw 0h ; Base 0:15
+    db 0h ; Base 16:23
+    db 10011100b ; Access
+    db 11001111b ; {Flags, Segment limit 16:19}
+    db 0h ; Base 24:31
+
+; Offset 0x10. Relevant to DS, SS, ES, FS, GS
+gdt_data:
+    dw 0ffffh ; Segment limit 0:15
+    dw 0h ; Base 0:15
+    db 0h ; Base 16:23
+    db 10010011b ; Access
+    db 11001111b ; {Flags, Segment limit 16:19}
+    db 0h ; Base 24:31
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
+
+[BITS 32] ; Code under here is 32 bits
+start32:
+.setup_segments_and_stack:
+    mov ax, DATA_SEGMENT
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+
+    mov ebp, 0200000h
+    mov esp, ebp
+
+.infinite_loop:
+    jmp $
+
+; Sector Padding
 
 times 510 - ($ - $$) db 0 ; Fill sector
-dw AA55h ; Add bootable sector signature (reversed because of endianity)
-
-buffer:
+dw 0aa55h ; Add bootable sector signature (reversed because of endianity)
