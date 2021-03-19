a6636cf3fd30b1d6332cffbe78a69b406b13b030
Dan Arad <dan1994@gmail.com>
2021-03-19 17:49:49 +0200

    Assume BPB is required and set segment registers
    We now changed ORG to 0, and set DS and ES registers to 0x7c0 instead.
    We also setup the stack to start at 0x7c00.
    
    We now assume a BPB block needs to be located at the start of the
    bootloader, and add it in a way that jumps to our code.

--- bootloader/boot.asm
+++ bootloader/boot.asm
@@ -1,7 +1,37 @@
-ORG 0x7c00 ; Offset all labels by the address where the bootloader is loaded
+ORG 0x0 ; Set offset of addresses to 0 and use segment registers instead
 BITS 16 ; Work in 16 bit mode
 
+; BIOS Parameter Block
+
+; Some BIOSes will expect a BPB (BIOS Parameter Block). It has to start with a
+; jmp and a nop. The jmp will go to an intermediate step that will do another
+; jmp, this time setting the code segment to the correct value.
+bpb:
+    jmp short bpb_trampoline
+    nop
+
+times 33 db 0 ; Dummy content for BPB fields
+
+bpb_trampoline:
+    jmp 0x7c0:start ; Sets code segment to where the bootloader is loaded
+
+; Main Code
+
 start:
+    cli ; Clear interrupts
+
+    ; Setup segments
+    mov ax, 0x7c0
+    mov ds, ax ; DS = 0x7c00
+    mov es, ax ; ES = 0x7c00
+
+    ; Setup stack
+    mov ax, 0x0
+    mov ss, ax ; SS = 0x0
+    mov sp, 0x7c00
+
+    sti ; Enable interrupts
+
     mov si, message
     call print
     jmp $ ; Infinite loop
