bc5dde640cbf393cba90a0c0b959a3e54ab7c6db
Dan Arad <dan1994@gmail.com>
2021-10-02 15:58:37 +0300

    Make sure `kernel_heap` is initialized before any other global variable.

--- include/kernel/memory/kernel_heap.hpp
+++ include/kernel/memory/kernel_heap.hpp
@@ -2,6 +2,6 @@
 
 #include "memory/heap.hpp"
 
-extern memory::Heap& kernel_heap;
+extern memory::Heap* kernel_heap;
 
 extern "C" memory::Heap& get_kernel_heap();
--- src/kernel/memory/kernel_heap.cpp
+++ src/kernel/memory/kernel_heap.cpp
@@ -1,13 +1,15 @@
 #include "memory/kernel_heap.hpp"
 
-memory::Heap& kernel_heap = get_kernel_heap();
+memory::Heap* kernel_heap = &get_kernel_heap();
 
 memory::Heap& get_kernel_heap() {
     std::byte* base_address = reinterpret_cast<std::byte*>(0x100000);
     constexpr size_t MAX_SIZE = 100 * 1024 * 1024;
     constexpr size_t BLOCK_SIZE = 4 * 1024;
 
-    static memory::Heap kernel_heap(base_address, MAX_SIZE, BLOCK_SIZE);
+    static memory::Heap internal_kernel_heap(base_address, MAX_SIZE,
+                                             BLOCK_SIZE);
+    kernel_heap = &internal_kernel_heap;
 
-    return kernel_heap;
+    return internal_kernel_heap;
 }
