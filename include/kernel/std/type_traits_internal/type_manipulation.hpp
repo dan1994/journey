4228212a0f42008099c702adb6886e217255080c
Dan Arad <dan1994@gmail.com>
2021-11-21 10:16:31 +0200

    Implement `std::move`

--- include/kernel/std/type_traits_internal/type_manipulation.hpp
+++ include/kernel/std/type_traits_internal/type_manipulation.hpp
@@ -40,4 +40,22 @@ struct remove_volatile<volatile T> {
     typedef T type;
 };
 
+template <class T>
+struct remove_reference {
+    typedef T type;
+};
+
+template <class T>
+struct remove_reference<T&> {
+    typedef T type;
+};
+
+template <class T>
+struct remove_reference<T&&> {
+    typedef T type;
+};
+
+template <class T>
+using remove_reference_t = typename remove_reference<T>::type;
+
 }  // namespace std
