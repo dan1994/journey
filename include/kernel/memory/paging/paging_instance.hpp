273f77e2be73960feb2bab05ba9b73b04fba9ba3
Dan Arad <dan1994@gmail.com>
2022-01-29 16:23:33 +0200

    Implement virtual to physical mapping.

--- include/kernel/memory/paging/paging_instance.hpp
+++ include/kernel/memory/paging/paging_instance.hpp
@@ -7,6 +7,20 @@ namespace memory::paging {
 
 class PagingInstance final {
    public:
+    struct Flags {
+        PriviledgeLevel priviledge_level;
+        AccessType access_type;
+    };
+
+    /**
+     * Initialize a page directory and its page tables with the given
+     * configurations.
+     * @param page_directory_flags Flags to be passed to the page directory
+     * entries.
+     * @param page_table_flags Flags to be passed to the page tables entries.
+     * @param initialization_mode Whether to initialize with 1:1 mapping or have
+     * all page tables point to page 0.
+     */
     explicit PagingInstance(
         const PageDirectoryEntry::Flags& page_directory_flags,
         const PageTableEntry::Flags& page_table_flags,
@@ -14,9 +28,25 @@ class PagingInstance final {
 
     ~PagingInstance();
 
+    /**
+     * Get the page directory.
+     * @return a const reference to the page table.
+     */
     const PageDirectory& get_directory() const;
 
+    /**
+     * Map a virtual address to a physical address. Addresses will be aligned.
+     * @param virtual_address The virtual address to map.
+     * @param physical_address The physical address to map to.
+     * @param flags Flags to set for the page.
+     */
+    void map(const void* virtual_address, const void* physical_address,
+             const Flags& flags);
+
    private:
+    PageDirectoryEntry& get_page_directory_entry(const void* virtual_address);
+    PageTableEntry& get_page_table_entry(const void* virtual_address);
+
     static PageTable* initialize_page_tables(
         const PageTableEntry::Flags& flags,
         PageTable::InitializationMode initialization_mode);
--- src/kernel/memory/paging/paging_instance.cpp
+++ src/kernel/memory/paging/paging_instance.cpp
@@ -1,11 +1,13 @@
 #include "memory/paging/paging_instance.hpp"
 
+#include <cassert>
 #include <new>
 
 #include "memory/paging/paging.hpp"
 #include "utilities/memory.hpp"
 
 namespace memory::paging {
+
 PagingInstance::PagingInstance(
     const PageDirectoryEntry::Flags& page_directory_flags,
     const PageTableEntry::Flags& page_table_flags,
@@ -19,6 +21,53 @@ PagingInstance::~PagingInstance() {
 
 const PageDirectory& PagingInstance::get_directory() const {
     return directory_;
+}
+
+void PagingInstance::map(const void* virtual_address,
+                         const void* physical_address, const Flags& flags) {
+    PageDirectoryEntry& pde = get_page_directory_entry(virtual_address);
+    if (!pde.is_present()) {
+        assertm(false, "Can't map address because of non-present page table");
+    }
+
+    PageTableEntry& pte = get_page_table_entry(virtual_address);
+
+    pte.set_page_address(reinterpret_cast<const std::byte*>(physical_address));
+
+    if (flags.access_type == AccessType::READ_WRITE) {
+        pte.enable_writing();
+        pde.enable_writing();
+    } else {
+        pte.disable_writing();
+    }
+
+    if (flags.priviledge_level == PriviledgeLevel::USER) {
+        pte.enable_user_access();
+        pde.enable_user_access();
+    } else {
+        pte.disable_user_access();
+    }
+
+    pte.mark_present();
+}
+
+PageDirectoryEntry& PagingInstance::get_page_directory_entry(
+    const void* virtual_address) {
+    const size_t offset = reinterpret_cast<unsigned int>(virtual_address) /
+                          (PageTable::NUMBER_OF_ENTRIES * PAGE_SIZE_IN_BYTES);
+
+    return directory_[offset];
+}
+
+PageTableEntry& PagingInstance::get_page_table_entry(
+    const void* virtual_address) {
+    const PageDirectoryEntry& pde = get_page_directory_entry(virtual_address);
+
+    const size_t offset = reinterpret_cast<unsigned int>(virtual_address) /
+                          PAGE_SIZE_IN_BYTES % PageTable::NUMBER_OF_ENTRIES;
+
+    PageTable page_table = pde.get_page_table();
+    return page_table[offset];
 }
 
 PageTable* PagingInstance::initialize_page_tables(
