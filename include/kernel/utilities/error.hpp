b84c4db00a4b0ac2c8bcffa2f6cec0c8764dd153
Dan Arad <dan1994@gmail.com>
2022-03-12 17:24:39 +0200

    Fix error interface + document.

--- include/kernel/utilities/error.hpp
+++ include/kernel/utilities/error.hpp
@@ -1,13 +1,37 @@
 #pragma once
 
 #include <pair>
+#include <string>
 
 class Error final {
    public:
+    /**
+     * Create a new error with the given message.
+     *
+     * @param message The error message.
+     */
     Error(const char* message = Error::no_error_);
 
+    /**
+     * Convert the error to a bool.
+     *
+     * @return True iff the message is not empty.
+     */
     operator bool() const;
-    const char* to_string() const;
+
+    /**
+     * Get the message as a char array.
+     *
+     * @return The message.
+     */
+    const char* message() const;
+
+    /**
+     * Get the message as a std::string.
+     *
+     * @return The message.
+     */
+    std::string to_string() const;
 
    private:
     const char* message_;
