51f732a3a3b2369b887acfa0232d81df8df0f68e
Dan Arad <dan1994@gmail.com>
2023-11-18 13:42:38 +0200

    Refactor kernel implementation

--- include/kernel/kernel/kernel.hpp
+++ include/kernel/kernel/kernel.hpp
@@ -1,43 +1,14 @@
 #pragma once
 
-#include <memory>
-
 #include "drivers/storage/ata.hpp"
-#include "memory/allocation/allocator.hpp"
+#include "memory/allocation/block_heap.hpp"
 #include "memory/paging/paging.hpp"
 
-class Kernel final {
-   public:
-    /**
-     * Get the kernel singleton.
-     */
-    [[nodiscard]] static Kernel& get_kernel(allocator* allocator);
-
-    /**
-     * Dtor.
-     */
-    ~Kernel() = default;
-
-    // Deleted functions
-    Kernel(const Kernel&) = delete;
-    Kernel(Kernel&&) = delete;
-    Kernel& operator=(const Kernel&) = delete;
-    Kernel& operator=(Kernel&&) = delete;
-
-   private:
-    /**
-     * Ctor. Performs all kernel startup activities.
-     */
-    explicit Kernel(allocator* allocator);
-
-    // The kernel singleton.
-    static std::unique_ptr<Kernel> kernel_;
-
-    [[nodiscard]] static memory::paging::Paging initialize_kernel_paging(
-        allocator* allocator);
-
-    constexpr static size_t MAX_NUMBER_OF_DISKS = 4;
-
-    drivers::storage::ata::disk boot_disk_;
-    memory::paging::Paging kernel_paging_;
+struct kernel {
+    memory::allocation::block_heap::block_heap heap_implementation;
+    allocator heap;
+    drivers::storage::ata::disk boot_disk;
+    memory::paging::Paging kernel_paging;
 };
+
+kernel make();
--- src/kernel/kernel/kernel.cpp
+++ src/kernel/kernel/kernel.cpp
@@ -4,33 +4,48 @@
 
 #include "interrupts/idt.hpp"
 #include "logging/logger.hpp"
+#include "memory/allocation/allocator.hpp"
+#include "memory/layout.hpp"
 
-Kernel& Kernel::get_kernel(allocator* allocator) {
-    if (!kernel_) {
-        Kernel* const kernel =
-            reinterpret_cast<Kernel*>(malloc(allocator, sizeof(Kernel)));
-        kernel_ = std::unique_ptr<Kernel>(allocator, kernel);
-    }
+memory::paging::Paging initialize_kernel_paging(allocator *allocator);
 
-    return *kernel_;
-}
+kernel make() {
+    constexpr size_t HEAP_BLOCK_SIZE = 4096;
+    constexpr size_t HEAP_SIZE = 100 * 1024 * 1024;
+    constexpr size_t HEAP_BLOCKS = HEAP_SIZE / HEAP_BLOCK_SIZE;
+    memory::allocation::block_heap::block_heap heap_implementation =
+        memory::allocation::block_heap::make_block_heap(
+            reinterpret_cast<uint8_t *>(memory::Layout::KERNEL_HEAP),
+            reinterpret_cast<memory::allocation::block_heap::block_metadata *>(
+                memory::Layout::KERNEL_HEAP_ENTRY_TABLE),
+            HEAP_BLOCK_SIZE, HEAP_BLOCKS);
 
-Kernel::Kernel(allocator* allocator)
-    : boot_disk_(drivers::storage::ata::disk{
-          .bus = drivers::storage::ata::Bus::PRIMARY,
-          .port = drivers::storage::ata::Port::MASTER,
-          .mode = drivers::storage::ata::Mode::PIO,
-      }),
-      kernel_paging_(initialize_kernel_paging(allocator)) {
+    allocator heap =
+        memory::allocation::block_heap::make_allocator(&heap_implementation);
+
+    drivers::storage::ata::disk boot_disk{
+        .bus = drivers::storage::ata::Bus::PRIMARY,
+        .port = drivers::storage::ata::Port::MASTER,
+        .mode = drivers::storage::ata::Mode::PIO};
+
     interrupts::init();
     logging::debug("Initialized interrupts...");
 
-    memory::paging::load(kernel_paging_);
+    memory::paging::Paging kernel_paging = initialize_kernel_paging(&heap);
+
+    memory::paging::load(kernel_paging);
     memory::paging::enable_paging();
     logging::debug("Initialized paging...");
+
+    return kernel{
+        .heap_implementation = heap_implementation,
+        .heap = heap,
+        .boot_disk = boot_disk,
+        .kernel_paging = std::move(kernel_paging),
+    };
 }
 
-memory::paging::Paging Kernel::initialize_kernel_paging(allocator* allocator) {
+memory::paging::Paging initialize_kernel_paging(allocator *allocator) {
     WithError<memory::paging::Paging> paging_err = memory::paging::Paging::make(
         allocator,
         {
@@ -47,5 +62,3 @@ memory::paging::Paging Kernel::initialize_kernel_paging(allocator* allocator) {
 
     return std::move(paging_err.first);
 }
-
-std::unique_ptr<Kernel> Kernel::kernel_{nullptr, nullptr};
