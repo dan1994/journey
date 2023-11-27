cca86f60dcaf28cd9c8a40d266d245b32795fb3b
Dan Arad <dan1994@gmail.com>
2023-11-27 19:08:39 +0200

    Refactor pair
    Make it a simple struct and move structured bindings logic related to
    pair to its header.

--- include/kernel/std/internal/utility/structured_bindings.hpp
+++ include/kernel/std/internal/utility/structured_bindings.hpp
@@ -2,9 +2,6 @@
 
 #include <stddef.h>
 
-#include <pair>
-#include <type_traits>
-
 namespace std {
 
 template <typename T>
@@ -12,32 +9,5 @@ struct tuple_size;
 
 template <size_t I, typename T>
 struct tuple_element;
-
-template <typename T1, typename T2>
-struct tuple_size<std::pair<T1, T2>> : integral_constant<size_t, 2> {};
-
-template <size_t I, typename T1, typename T2>
-struct tuple_element<I, std::pair<T1, T2>> {
-    static_assert(I < 2, "std::pair has only 2 elements!");
-};
-
-template <typename T1, typename T2>
-struct tuple_element<0, std::pair<T1, T2>> {
-    using type = T1;
-};
-
-template <typename T1, typename T2>
-struct tuple_element<1, std::pair<T1, T2>> {
-    using type = T2;
-};
-
-template <size_t I, typename T1, typename T2>
-auto get(std::pair<T1, T2> pair) {
-    if constexpr (I == 0) {
-        return pair.first;
-    } else {
-        return pair.second;
-    }
-}
 
 }  // namespace std
